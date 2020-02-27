d764960fe888d02bcf7f87a9943f6381fc30d6ea
Substance0510 <Substance0510@gmail.com>
2020-02-27 20:12:50 +0300

    Немного отрефакторил код в picture1 и добавил восход солнца.

--- Lecture4_Graph/picture1.py
+++ Lecture4_Graph/picture1.py
@@ -12,43 +12,62 @@ def rectangle(x1, y1, x2, y2, pencolor, brush):
     gr.penColor(pencolor)
     gr.rectangle(x1, y1, x2, y2)
 
+
 def duga(xstart, xmax, xstep, ystart, ystep, yincrease):
-	""" Функция рисует округлые холмы и пологие склоны (дуги), в зависимости
+    """ Функция рисует округлые холмы и пологие склоны (дуги), в зависимости
 		от переданных значений шагов для координат X и Y. Координаты увеличиваются не
 		в равной степени, за что отвечает параметр yincrease. Максимальное значение Y
-		не учитывается. """
+		не учитывается.
+	"""
+    points_duga = []
     while xstart < xmax:
-        points.append((xstart, ystart))
+        points_duga.append((xstart, ystart))
         xstart += xstep
         ystart -= ystep
         ystep += yincrease
+    return points_duga
+
+
+def merge_lists(list1, list2):
+    """
+    Добавляем значения из списка 2 в конец списка 1.
+    """
+    for i in list2:
+        list1.append(i)
+    return list1
+
 
 def ptushka(xpt, ypt, size=1):
-	""" Функция рисует птушек, используя функцию рисования дуг (холмов) и
+    """
+		Функция рисует птушек, используя функцию рисования дуг (холмов) и
 		используя глобальную переменную для создания списка точек point, которая
 		создаёт пустой список в начале функции. Параметр size отвечает за размер
-		птушек по оси X и, как следствие, за угол между крыльями (размах)."""
-    global points
-    points = []
+		птушек по оси X и, как следствие, за угол между крыльями (размах).
+	"""
     gr.moveTo(xpt, ypt)
-    duga(xpt, xpt + 36*size, 3*size, ypt, 1, -0.5)
-    gr.polygon(points)
-    points = []
-    duga(xpt + 33*size, xpt + 74*size, 3*size, ypt + 16, 4, -0.4)
-    gr.polygon(points)
+    wing1 = gr.polygon(duga(xpt, xpt + 36 * size, 3 * size, ypt, 1, -0.5))
+    wing2 = gr.polygon(duga(xpt + 33 * size, xpt + 74 * size, 3 * size, ypt + 16, 4, -0.4))
+    return wing1, wing2
 
 
-# def polygon()
+def move_sun():
+    gr.moveObjectBy(sun, -1, -2)
+
 
 # раскраска полотна
 rectangle(0, 0, 1000, 150, '#FFE4C4', '#FFE4C4')
 rectangle(0, 150, 1000, 300, '#FFE4B5', '#FFE4B5')
-rectangle(0, 300, 1000, 450, '#FAEBD7', '#FAEBD7')
-rectangle(0, 450, 1000, 700, '#4169E1', '#4169E1')
-# солнце
+
+# солнышко, чтобы выходило из-за гор, но поверх неба.
 gr.brushColor('yellow')
 gr.penColor('yellow')
-gr.circle(500, 150, 50)
+sun = gr.circle(600, 230, 50)
+gr.onTimer(move_sun, 500)
+
+# продолжили раскраску полотна
+rectangle(0, 300, 1000, 450, '#FAEBD7', '#FAEBD7')
+rectangle(0, 450, 1000, 700, '#4169E1', '#4169E1')
+
 # первые горы
 gr.moveTo(0, 350)
 gr.brushColor('orange')
@@ -56,7 +75,7 @@ gr.penColor('orange')
 gr.penSize(5)
 points.append((0, 350))
 points.append((10, 320))
-duga(10, 190, 10, 320, 2, 1)
+merge_lists(points, duga(10, 190, 10, 320, 2, 1))
 points.append((200, 145))
 points.append((210, 165))
 points.append((400, 280))
@@ -65,12 +84,12 @@ points.append((540, 290))
 points.append((600, 220))
 points.append((650, 250))
 points.append((700, 210))
-duga(700, 800, 5, 210, 1, 0.4)
-duga(800, 850, 5, 110, 10, -2)
+merge_lists(points, duga(700, 800, 5, 210, 1, 0.4))
+merge_lists(points, duga(800, 850, 5, 110, 10, -2))
 points.append((890, 160))
 points.append((950, 120))
 points.append((970, 180))
-points.append((1000,250))
+points.append((1000, 250))
 gr.polygon(points)
 
 # Вторые горы
@@ -82,15 +101,15 @@ points.append((0, 490))
 points.append((0, 375))
 points.append((5, 375))
 points.append((15, 390))
-duga(20, 150, 9, 373, 20, -4)
+merge_lists(points, duga(20, 150, 9, 373, 20, -4))
 points.append((180, 390))
 points.append((230, 420))
 points.append((280, 350))
 points.append((350, 365))
 points.append((400, 400))
 points.append((500, 380))
-duga(505, 620, 9, 375, 12, -1.7)
-duga(620, 700, 10, 350, -10, 1)
+merge_lists(points, duga(505, 620, 9, 375, 12, -1.7))
+merge_lists(points, duga(620, 700, 10, 350, -10, 1))
 points.append((750, 350))
 points.append((800, 380))
 points.append((830, 340))
@@ -101,7 +120,7 @@ points.append((1000, 290))
 points.append((1000, 470))
 gr.polygon(points)
 
-# Третьи горы
+# третьи горы
 gr.moveTo(0, 700)
 gr.brushColor('#4F1946')
 gr.penColor('#4F1946')
@@ -110,18 +129,19 @@ points.append((0, 700))
 points.append((0, 375))
 points.append((110, 400))
 points.append((190, 520))
-duga(190, 400, 5.7, 520, -10, 0.3)
+merge_lists(points, duga(190, 400, 5.7, 520, -10, 0.3))
 points.append((470, 690))
 points.append((560, 600))
 points.append((610, 620))
-duga(615, 900, 8, 622, -5, 0.5)
-duga(895, 1010, 9, 500, 12, -1.7)
+merge_lists(points, duga(615, 900, 8, 622, -5, 0.5))
+merge_lists(points, duga(895, 1010, 9, 500, 12, -1.7))
 points.append((1000, 700))
 gr.polygon(points)
 
-# Птушки
+# птушки
 gr.brushColor('black')
 gr.penColor('black')
+
 ptushka(490, 250)
 ptushka(400, 230, 0.5)
 ptushka(510, 310, 0.7)
